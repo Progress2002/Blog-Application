c76fe1b1085ac66fa8a1d7af27ad0fdbc9067e2b
Progress2002 <ezeamakachisom2002@gmail.com>
2023-06-07 03:58:01 +0100

    Create custom methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,15 @@
 class Post < ApplicationRecord
-    belong_to :user, foreign_key: 'author_id'
+    belongs_to :user, foreign_key: 'user_id'
     has_many :comments
     has_many :likes
+
+    def update_counter(user_id)
+        user = User.find_by(id: user_id)
+        user.posts_counter = Post.where(user_id: user_id).count
+        user.save
+    end
+
+    def recent_comment
+        Comment.where(post_id: id).order(created_at: :desc).limit(5)
+    end
 end
