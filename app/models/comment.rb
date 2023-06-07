e38a3e1d6fdbe3f8a2c33f0604e6beaf43a9ef3c
Progress2002 <ezeamakachisom2002@gmail.com>
2023-06-07 04:02:02 +0100

    Fix linter errors

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,10 @@
 class Comment < ApplicationRecord
-    belongs_to :user, foreign_key: 'user_id'
-    belongs_to :post
+  belongs_to :user, foreign_key: 'user_id'
+  belongs_to :post
 
-    def update_counter(post_id)
-        post = Post.find_by(id: post_id)
-        post.comments_counter = Comment.where(post_id: post_id).count
-        post.save
-    end
+  def update_counter(post_id)
+    post = Post.find_by(id: post_id)
+    post.comments_counter = Comment.where(post_id: post_id).count
+    post.save
+  end
 end
